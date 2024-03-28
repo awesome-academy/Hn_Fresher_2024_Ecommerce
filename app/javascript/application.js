219383cc2035917479c1ec0e2a9464cba51b8109
hieu mai <mai.van.hieu@sun-asterisk.com>
2024-03-28 20:50:21 +0700

    init project and add template

---
+++ app/javascript/application.js
@@ -0,0 +1,20 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+//= require js/main
+//= require js/map
+//= require js/slick-custom
+//= require vendor/jquery/jquery-3.2.1.min
+//= require vendor/animsition/js/animsition.min
+//= require vendor/bootstrap/js/popper
+//= require vendor/bootstrap/js/bootstrap.min
+//= require vendor/select2/select2.min
+//= require vendor/daterangepicker/moment.min
+//= require vendor/daterangepicker/daterangepicker
+//= require vendor/slick/slick.min
+//= require vendor/parallax100/parallax100
+//= require vendor/MagnificPopup/jquery.magnific-popup.min
+//= require vendor/isotope/isotope.pkgd.min
+//= require vendor/sweetalert/sweetalert.min
+//= require vendor/perfect-scrollbar/perfect-scrollbar.min
